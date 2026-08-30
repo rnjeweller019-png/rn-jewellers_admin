876d129785eafa1dba149b658d9bc8f929292971
Naitik Dhiman <naitikdhiman06@gmail.com>
2026-08-30 10:08:27 +0530

    Update APPS_SCRIPT_URL to deployment AKfycbwhLkDy7amTSsSEQpRgR73KaUmuvuDIEnUeYlLV_U5ZtEN-yDez9lmN12S9bfJ0_r66

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
   // Replace this with your Google Apps Script Web App URL after deployment
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwrv1D6KOSJyz2oCooBbX44PteNDtVLANe7XXNgdBZoQqobVmXnc1RN58ReLgkybuB_/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwhLkDy7amTSsSEQpRgR73KaUmuvuDIEnUeYlLV_U5ZtEN-yDez9lmN12S9bfJ0_r66/exec",
   
   // OneSignal App ID for Push Notifications (Optional)
   ONESIGNAL_APP_ID: "91a28970-9e1b-4343-a379-de2a1923e7a7",
